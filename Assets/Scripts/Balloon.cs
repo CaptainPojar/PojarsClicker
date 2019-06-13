1707afa0e9a17bcb7595159445ce21556ab78682
CaptainPojar <zheck14@mail.ru>
2019-06-13 23:19:02 +0300

    commit project

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    [SerializeField] private float _scoreScaler;
+    [SerializeField] private float _scaleSpeed;
+    [SerializeField] private Vector3 _minScale;
+    [SerializeField] private Vector3 _maxScale;
+    [SerializeField] private Color[] _colors;
+
+    void Start()
+    {
+        transform.localScale = _minScale;
+        if (_colors.Length != 0)
+        {
+            Color color = _colors[Random.Range(0, _colors.Length)];
+            gameObject.GetComponent<SpriteRenderer>().color = color;
+        }
+    }
+
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _maxScale, _scaleSpeed * Time.deltaTime);
+        if (transform.localScale.x > _maxScale.x * 0.9f)
+        {
+            var scores = -(_scoreScaler * transform.localScale.x);
+            GameStarter.SetScore(scores);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        var scores = _scoreScaler * transform.localScale.x;
+        GameStarter.SetScore(scores);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameStarter : MonoBehaviour
+{
+    [SerializeField] private GameObject _balloonPrefab;
+    [SerializeField] private float _maxScores;
+    private GameObject _startButton;
+    private GameObject _resultBoard;
+    private static float _totalScores;
+    private static Text _scoreBoard;
+    private Text _timerBoard;
+    private float _timer;
+
+    [UsedImplicitly]
+    public void StartGame()
+    {
+        Debug.Log("Game started!");
+        _startButton = GameObject.Find("StartGameButton");
+        _resultBoard = GameObject.Find("ResultBoard");
+        _timerBoard = GameObject.Find("Time").GetComponent<Text>();
+        _scoreBoard = GameObject.Find("Scores").GetComponent<Text>();
+        _startButton.SetActive(false);
+        _resultBoard.SetActive(false);
+
+        GamePlay();
+    }
+
+    public void ResultBoard(string timerBoardText)
+    {
+        _resultBoard.SetActive(true);
+        _startButton.SetActive(true);
+        _resultBoard.GetComponent<Text>().text = "You Win!\nTime: " + timerBoardText + " sec";
+        var gameObjects = GameObject.FindObjectsOfType(typeof(Balloon));
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Destroy(gameObjects[i]);
+        }
+        StopCoroutine(SpawnBalls());
+    }
+
+    private void GamePlay()
+    {
+        _totalScores = 0;
+        _timerBoard.text = "0";
+        _timer = Time.time;
+        StartCoroutine(SpawnBalls());
+    }
+
+    void Update()
+    {
+        if (_startButton != null && !_startButton.activeSelf)
+        {
+            _timer += Time.deltaTime;
+            int t = (int) (_timer % 60);
+            _timerBoard.text = t.ToString();
+        }
+    }
+
+    private IEnumerator SpawnBalls()
+    {
+        while (_maxScores > _totalScores)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Vector3 randomPosition = new Vector3(Random.Range(-2.2f, 2.2f), Random.Range(-4f, 5f), 0);
+            Quaternion randomRotation = transform.rotation * Quaternion.Euler(0, 0, Random.Range(0, 190));
+            Instantiate(_balloonPrefab, randomPosition, randomRotation);
+        }
+
+        ResultBoard(_timerBoard.text);
+    }
+
+    public static void SetScore(float scores)
+    {
+        if (_scoreBoard != null)
+        {
+            _totalScores += scores;
+            _scoreBoard.text = Math.Round(_totalScores).ToString();
+        }
+    }
+}
